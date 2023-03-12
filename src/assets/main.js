435ce606e6d6c62b60b54a5f747af4e43cc992a8
Josue <sjosue5082002@gmail.com>
2023-03-12 17:37:21 -0500

    6.3 Desplegando el proyecto

--- src/assets/main.js
+++ src/assets/main.js
@@ -25,7 +25,7 @@ async function fetchData(urlApi) {
       <div class="group relative"> 
          <div 
            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-           <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+           <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
          </div> 
          <div class="mt-4 flex justify-between"> 
            <h3 class="text-sm text-gray-700"> 
@@ -36,7 +36,8 @@ async function fetchData(urlApi) {
        </div> 
      `).slice(0,4).join('')} 
      `; 
-   } catch { 
-  
+      content.innerHTML = view;
+   } catch (error) { 
+     console.log(error);
    } 
 })();  
